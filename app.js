df8313c5b920b18dc830acad8025eb9bc10d35e9
salvadoragron <joe.rosenfield.jr@gmail.com>
2018-03-27 22:12:31 +0100

    answers 3-5

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Check the console inside chrome developer tools to see me running
 
 console.log("Hello I'm running")
+//1
+console.log("Richard Gill")
 
+//2
 var firstName = "Joe";
 var lastName = "Rosenfield";
 
@@ -9,3 +12,21 @@ var firstNamelastName = function (firstName, lastName) {
 	console.log(firstName + " " + lastName);
 }
 firstNamelastName(firstName, lastName)
+
+//3
+var lastNamefirstName = function (lastName, firstName) {
+	console.log(lastName + ", " + firstName);
+}
+lastNamefirstName(lastName, firstName)
+
+//4
+console.log(firstName.length)
+
+//5
+var people = ['Jack', 'John', 'James']
+
+for (var i = people.length - 1; i >= 0; i--) {
+		console.log(people[i])
+	}
+
+
