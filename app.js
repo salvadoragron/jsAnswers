2a9b11c6bb2c1a24a27924ab96b467fc54f88742
salvadoragron <joe.rosenfield.jr@gmail.com>
2018-03-27 22:33:08 +0100

    Answers 6-8

--- app.js
+++ app.js
@@ -29,4 +29,41 @@ for (var i = people.length - 1; i >= 0; i--) {
 		console.log(people[i])
 	}
 
+//6
+var nameSlice = function(name) {
+	console.log(name.slice(1))
+}
+nameSlice(firstName)
 
+//7
+var sliceJName = function(name){
+	if(name.charAt(0) == 'J') {
+		console.log(name.slice(1))
+	}
+	else {
+		console.log(name)
+	}
+}
+console.log("Calling sliceJName function on my first name which starts with a J")
+sliceJName(firstName)
+
+console.log("Calling sliceJName function on my last name which does not start with a J")
+sliceJName(lastName)
+
+//8
+var sliceJAndRName = function(name){
+	if(name.charAt(0) == 'J') {
+		console.log(name.slice(1))
+	}
+	else if(name.charAt(0) == 'R') {
+		console.log(name.slice(0, -2))
+	}
+	else {
+		console.log(name)
+	}
+}
+console.log("Calling sliceJAndRName function on my first name which starts with a J")
+sliceJAndRName(firstName)
+
+console.log("Calling sliceJAndRName function on my last name which does not start with a J but with an R")
+sliceJAndRName(lastName)
