c4567ca492fb26929daeafb5f54fbbbacbf2cae5
salvadoragron <joe.rosenfield.jr@gmail.com>
2018-03-27 22:02:14 +0100

    answers 1 and 2

---
+++ app.js
@@ -0,0 +1,11 @@
+// Check the console inside chrome developer tools to see me running
+
+console.log("Hello I'm running")
+
+var firstName = "Joe";
+var lastName = "Rosenfield";
+
+var firstNamelastName = function (firstName, lastName) {
+	console.log(firstName + " " + lastName);
+}
+firstNamelastName(firstName, lastName)
